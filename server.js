92f17a447b53e8348acd0f0133b0cb0b25d6a2a8
Daniel Hegner <daniel@hegner.se>
2015-12-21 19:55:28 +0100

    New message

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var say = require('say');
 
-var text = process.env.TEXT || 'Say hello world to my little friend';
+var text = process.env.TEXT || 'Say hello world to my little friend. Kan du prata svenska?';
 
 say.speak(null, text);
